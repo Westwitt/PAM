8f7256c842b2fd67b40bb4b2c0e7d8f6f8657a5b
Bruno Oliveira <68453992+TheBrunno@users.noreply.github.com>
2023-06-13 22:47:31 -0300

    Update App.js

--- hollow-knight/App.js
+++ hollow-knight/App.js
@@ -1,12 +1,39 @@
-import { StyleSheet, Text, View, Image } from 'react-native';
-import { dados } from './data';
+import { StyleSheet, Text, View, Image, Modal } from "react-native";
+import { dados } from "./data";
+import { FlatList } from "react-native-web";
+import { useFonts } from "expo-font";
+import { useState } from "react";
 
 export default function App() {
+  const [loaded] = useFonts({
+    TrajanPro: require("./assets/fonts/TrajanPro-Regular.ttf")
+  })
+
+  const [visible, setVisible] = useState(false);
+
   return (
     <View style={styles.container}>
-      <Text>asasas</Text>
-      <Image source={{uri: dados[5].imagem}}
-       style={styles.imagem} />
+      <Text style={styles.text}>
+        Hollow Knight
+      </Text>
+      <Image source={require("./assets/Hr.png")} style={styles.hr} />
+      <FlatList style={styles.flat}
+        data={dados}
+        keyExtractor={({id}, index) => id}
+        renderItem={({item, index}) => (
+          <View style={styles.item} onPress={renderModel(index)}>
+            <Text style={styles.normalText}>{item.nome}</Text>
+            <Image source={{uri: item.imagem}} style={styles.imagem} />
+          </View>
+        )}
+      
+      >
+      </FlatList>
+      <Modal visible={visible} animationType="slide" style={styles.modal} transparent>
+          <View style={styles.modal}>
+            <Text style={styles.normalText}>{dados[0].nome}</Text>
+          </View>
+      </Modal>
     </View>
   );
 }
@@ -14,13 +41,44 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#1a1e23",
   },
   imagem: {
     width: 200,
     height: 200,
-    resizeMode: 'contain'
+    resizeMode: "contain",
+  },
+  flat: {
+    width: "100%", 
+    padding: 10,
+  },
+  item: {
+    alignItems: "center",
+    marginBottom: 30
+  },
+  text: {
+    color: "#e6e6e6",
+    fontFamily: "TrajanPro",
+    fontSize: "150%",
+    marginTop: 30,
+    marginBottom: 10
+  },
+  normalText: {
+    color: "#e6e6e6",
+    fontFamily: "TrajanPro",
+    fontSize: "1.01em",
+    marginBottom: 5
+  },
+  hr: {
+    width: "90vw",
+    height: 25
+  },
+  modal: {
+    backgroundColor: "#1a1e23",
+    width: "100vw",
+    height: "100vh"
   }
 });
