9a41bc74c7b0937491292b4d0e0b05ddd3446670
Bruno Oliveira <68453992+TheBrunno@users.noreply.github.com>
2023-12-03 15:53:17 -0300

    the places screen

--- ZooKids/src/pages/Places/Index.js
+++ ZooKids/src/pages/Places/Index.js
@@ -1,30 +1,24 @@
-import React, { useRef } from 'react';
-import { Image, View, Animated } from 'react-native';
-import { PanGestureHandler } from 'react-native-gesture-handler';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { View, Image, Dimensions } from 'react-native';
+import ImageZoom from 'react-native-image-pan-zoom';
 
-const MapScreen = () => {
- const translateX = useRef(new Animated.Value(0)).current;
- const translateY = useRef(new Animated.Value(0)).current;
+import styles from './styles';
 
- const onGestureEvent = Animated.event(
-  [{ nativeEvent: { translationX: translateX, translationY: translateY } }],
-  { useNativeDriver: true },
- );
-
- return (
-  <View style={{ flex: 1 }}>
-    <PanGestureHandler onGestureEvent={onGestureEvent}>
-      <Animated.Image
-        style={{
-          width: '100%',
-          height: '100%',
-          transform: [{ translateX }, { translateY }],
-        }}
-        source={{ uri: 'https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/34a42e114485041.603c8e648ace2.png' }}
-      />
-    </PanGestureHandler>
-  </View>
- );
-};
-
-export default MapScreen;
+export default function Places() {
+  return (
+    <View style={styles.container}>
+      <ImageZoom
+        enableImageZoom={true}
+        cropWidth={Dimensions.get('window').width}
+        cropHeight={Dimensions.get('window').height}
+        imageWidth={1284}
+        imageHeight={896}
+        enableCenterFocus={false}
+        
+      >
+        <Image source={require('../../../assets/imgs/maps.png')} style={{width:1284, height:896}} />
+      </ImageZoom>
+    </View>
+  );
+}
